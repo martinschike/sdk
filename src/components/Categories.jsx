f024619f4d2a0831f5695cc492f1871de92aab8e
MARTINS OKAFOR <okaformartinschinwike@gmail.com>
2022-08-18 11:22:36 +0100

    frames are distintively defined and more styling

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -24,19 +24,25 @@ const Categories = ({
       >
         UnInstalled
       </button>
-      <header>
-        <h3>{active}</h3>
-        <h3>{totalSDK}</h3>
-      </header>
-      {categories.map((c, i) => {
-        const sdks = getSDKs(c, active);
-        return (
-          <div key={i} className="cat">
-            <h2 className="categories">{c} </h2>
-            {sdks.length ? <SDKList sdks={sdks} /> : ""}
-          </div>
-        );
-      })}
+      <div className="cover">
+        <header>
+          <h3>{active}</h3>
+          <h3>{totalSDK}</h3>
+        </header>
+        <div className="set">
+          {categories.map((c, i) => {
+            const sdks = getSDKs(c, active);
+            return (
+              <div key={i}>
+                <div className="content">
+                  <h2 className="categories">{c} </h2>
+                  <span>{sdks.length ? <SDKList sdks={sdks} /> : ""}</span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
